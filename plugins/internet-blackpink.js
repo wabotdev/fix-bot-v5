2b4b1fe51e7088cf468f14a47e896ca44a25babc
wabotdev <120768734+wabotdev@users.noreply.github.com>
2023-02-14 17:33:47 +0700

    Update internet-blackpink.js

--- plugins/internet-blackpink.js
+++ plugins/internet-blackpink.js
@@ -7,7 +7,7 @@ fetch('https://raw.githubusercontent.com/arivpn/dbase/master/kpop/blekping.txt')
 let handler = async (m, { conn }) => {
     let img = bpink[Math.floor(Math.random() * bpink.length)]
     if (!img) throw img
-    await conn.sendFile(m.chat, img, '', '© nih Blackpinknya jangan lupa subscribe channel WH MODS DEV 👍', m, 0, { thumbnail: await (await fetch(img)).buffer() })
+    await conn.sendFile(m.chat, img, '', '© nih Blackpinknya 👍', m, 0, { thumbnail: await (await fetch(img)).buffer() })
 }
 handler.help = ['blackpink']
 handler.tags = ['internet']
