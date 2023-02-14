4b325f8b50c9c97c2af38e8e542e45672a85abe3
wabotdev <120768734+wabotdev@users.noreply.github.com>
2023-02-14 17:23:37 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -18,10 +18,10 @@ let str = `${global.wm}
 let wibu = `https://i.ibb.co/P5BBzbf/286637238-392674839545088-948545114162206238-n.jpg` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Sewa Bot','.sewa', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
-    mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    mediaUrl: "https://Instagram.com/andiigndr29",
+    mediaType: "PICTURE",
+    description: "https://Instagram.com/andiigndr29", 
+    title: '𝓦𝓸𝓷𝓰 𝓤𝓻𝓲𝓹',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sig
